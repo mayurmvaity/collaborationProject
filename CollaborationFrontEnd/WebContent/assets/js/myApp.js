9d5e03d729a8887a3e7afaca66adfebdade636dd
mayurmvaity <mayurmvaity@hotmail.com>
2017-06-06 03:19:40 +0530

    Some minor changes; running.

--- CollaborationFrontEnd/WebContent/assets/js/myApp.js
+++ CollaborationFrontEnd/WebContent/assets/js/myApp.js
@@ -1,11 +1,87 @@
-/**
- * 
- */
+$( document ).ready(function(){
 
-var mcss = function(){
-	$(document).ready(function() {
-		$(".dropdown-button").dropdown();
+	$(".dropdown-button").dropdown({
+		
+		 inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: true,
+	      belowOrigin: true
+		
 	});
-};
+	
 
-mcss();
+	$(".dropdown-job").dropdown({
+		
+		 inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: true,
+	      belowOrigin: true
+		
+	});
+	
+	
+	$(".dropdown-blog").dropdown({
+		
+		 inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: true,
+	      belowOrigin: true
+		
+	});   
+	
+	$(".dropdown-event").dropdown({
+		
+		 inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: true,
+	      belowOrigin: true
+		
+	});
+	
+	$(".dropdown-user").dropdown({
+		
+		 inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: true,
+	      belowOrigin: true
+		
+	});
+	
+	// user stuff
+	
+	$(".dropdown-UserForum").dropdown({
+		
+		 inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: true,
+	      belowOrigin: true
+		
+	});
+	
+	$(".dropdown-Useruser").dropdown({
+		
+		 inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: true,
+	      belowOrigin: true
+		
+	});
+	
+	$(".dropdown-UserBlog").dropdown({
+		
+		 inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: true,
+	      belowOrigin: true
+		
+	});
+	        
+});
